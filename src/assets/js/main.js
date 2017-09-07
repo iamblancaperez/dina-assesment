ac0b1b430018f0786e2bea69761164fafd0ae600
Blanca Perez <blankp@gmail.com>
2017-09-06 22:10:34 -0300

    Added function to create user

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,34 +1,11 @@
-/*$(document).ready(function($) {
-	var location = window.location;
-	var mainContainer = document.getElementById("main-container");
-	switch (location.pathname) {
-		case "/new":
-			var user = new User();
-			user.renderForm(mainContainer);
-			break;
-		case "/edit":			
-			getUser().then(function(userJSON){
-				var user = new User(userJSON.first_name, userJSON.last_name, userJSON.status);
-				user.renderForm(mainContainer, "edit");
-			});			
-			break;
-		default:
-			getUsers().then(function(usersJSON){
-				var users = new Pagination(usersJSON, 0);
-				users.setPagination(mainContainer);				
-			});
-			break;
-	}
-});
-*/
 $(document).ready(function($) {
 	var mainContainer = document.getElementById("main-container");
+	var createUser = document.getElementById("create-user-link");
 	Router.config({ mode: 'history'}); // configuration	
 	Router.navigate(); // returning the user to the initial state
 	// adding routes
 	Router
 	.add(/(.*)\/edit/, function() {
-		console.log('sdasdasd')
 		getUser().then(function(userJSON){
 			var user = new User(userJSON.first_name, userJSON.last_name, userJSON.status);
 			user.renderForm(mainContainer, "edit");
@@ -46,6 +23,9 @@ $(document).ready(function($) {
 	})
 	.check('/').listen();
 
-// forwarding
-//Router.navigate('/22');	
+	createUser.addEventListener('click',function(e){
+		e.preventDefault();
+		Router.check('/new').listen();
+	});
+
 });
